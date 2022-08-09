94e443bc0197b4bc001033bff38f43d17f787454
Pisarev Danila <100018059+jobdn@users.noreply.github.com>
2022-08-09 07:47:15 +0300

    feat: Промис - это объект, который мы создаем с исполнителем
    Исполнитель - это колбэк с другугими колбэками. Главная задача исполнителя -
    дать какой-то результат, сейчас или через какое-то время. Он должен
    завершить промис или положительно, или отрицательно.

--- promise/src/index.js
+++ promise/src/index.js
@@ -1,3 +1,34 @@
-function newFunction() {
-  alert("newFunction!!!");
+function loadScript(src, cb) {
+  const script = document.createElement("script");
+  script.src = src;
+
+  script.onload = () => cb(null, script);
+
+  script.onerror = () =>
+    cb(new Error(`The loading script ${script.src} failed`));
+
+  document.body.append(script);
 }
+
+// PROMISE
+const prom1 = new Promise((res, rej) => {
+  setTimeout(() => {
+    rej(new Error(":C"));
+  }, 1000);
+});
+
+prom1
+  .finally(() => {
+    console.log("Цепочка выполняется последовательно");
+    // finally не принимает никаких аргументов и пропускает через себя промис, который дальше используется либо в then либо в catch
+  })
+  .then((value) => {
+    console.log(value);
+    console.log(
+      "Но, в зависимости от состояния промиса, срабатывает либо then либо catch"
+    );
+  })
+  .catch((error) => console.log(error))
+  .finally(() => {
+    console.log("finally выполняется в любом случае");
+  });
