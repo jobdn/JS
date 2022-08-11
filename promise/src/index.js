415e34a736db3c6dd47c5377daeb049efc90d6d1
Pisarev Danila <danila.pisarev@payture.com>
2022-08-11 08:20:44 +0300

    --- LessonEnd: Цепочка промисов https://learn.javascript.ru/promise-chaining
    Мы говорим операции: ты пока выполняйся, a результат твоей работы мы
    используем потом или подождем пока ты выполнишься
    
    То есть эта операция будет выполняться, пока другой код работает.

--- promise/src/index.js
+++ promise/src/index.js
@@ -11,25 +11,20 @@ function loadScript(src) {
   });
 }
 
-fetch("./server/users.json")
-  .then(response => {
-    /**
-     * Этот объект возвращается до того, как сервер полностью загружен.
-     * 
-     * А вот response.json() возвращает промис, который выполняется, 
-     * когда данные полностью загружены с сервера.  
-     */
-    console.log("response: ", response);
-    return response.json()
-  })
-  .then(users => users[0])
-  .then(user => fetch(`https://api.github.com/users/${user.name}`))
+function loadGithubUserImage() {
+  const githubUser = prompt("Enter github user name: ", "jobdn");
+
+  fetch(`https://api.github.com/users/${githubUser}`)
   .then(gitHubResponse => gitHubResponse.json())
-  .then(gitHubUser => {
-    console.log(gitHubUser);
-    const imgEl = document.createElement("img");
-    imgEl.src = gitHubUser.avatar_url;
-    document.body.append(imgEl);
-    imgEl.style.borderRadius = "50%";
-    imgEl.style.border = "3px solid orange"
-  })
+  .then(createAndAppendImg)
+}
+
+function createAndAppendImg(gitHubUser) {
+  const imgEl = document.createElement("img");
+  imgEl.src = gitHubUser.avatar_url;
+  document.body.append(imgEl);
+  imgEl.style.borderRadius = "50%";
+  imgEl.style.border = "3px solid orange"
+}
+
+loadGithubUserImage()
