45bb68216aa9f304696a9011dab15ea6e8cec4db
Pisarev Danila <danila.pisarev@payture.com>
2022-08-12 13:13:49 +0300

    Перехват ошибок

--- promise/src/index.js
+++ promise/src/index.js
@@ -11,11 +11,20 @@ function loadScript(src) {
   });
 }
 
-let promise = Promise.resolve();
-
-/**
- * Колбэк внутри потребителя будет помещен в ОЧЕРЕДЬ МИКРОТАСОК и выполнен после выполнения синхронного кода.
- */
-promise.then(() => console.log("Промис обработан"));
-
-console.log("СИНХРОННЫЙ КОД ОТРАБОТАЛ");
+fetch("https://error_url.com")
+  .then((res) => res.json())
+  .then(() => {
+    console.log("Этот then выполняться не будет")
+  })
+  .then(() => {
+    console.log("Этот then выполняться не будет")
+  })
+  .then(() => {
+    console.log("Этот then выполняться не будет")
+  })
+  .catch((error) => {
+    console.log("Обработчик")
+    console.log("будет выполнен, ")
+    console.log("потому что fetch вернет промис с ошибкой!")
+    console.log("ERROR: ", error); 
+  })
